b1b1420defa3e4526a96a0534f192876f2c9cf6c
Vladislav Bogomolov <vladson4ik@gmail.com>
2014-04-28 00:00:46 +0400

    CacheMatrix implementation. Calculations should work properly

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inverse computation - time consuming task. Using these two functions it is easy to
+## get rid of inverse computation because it is computed just once.
 
-## Write a short comment describing this function
-
+## This one returns special list containig special functions for get-set
+## of numeric matrix an inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) m <<- inv
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This one returns inverse of matrix if possible and caches the result for \
+## further use.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+  }
+  data <- x$get()
+  ## Check if matrix is square and of full rank and store an error msg if not
+  if(dim(data)[1]!=dim(data)[2]) {
+      m<-("Not invertible")
+      x$setInverse(m)
+      return(m)
+  }
+  ## Return a matrix that is the inverse of 'x'
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
 }
